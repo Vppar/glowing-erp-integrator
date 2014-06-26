4613decc51a05f4a053283718d9262f1265e87c7
rennannogarotto <rennan.nogarotto@tuntscorp.com>
2014-06-26 14:45:48 -0300

    setup

--- test/listeners/listeners.js
+++ test/listeners/listeners.js
@@ -1,1 +1,19 @@
-//TODO teste  [function listeners()]
+var expect = require('chai').expect;
+
+var MODULE_PATH = '../../lib/listeners';
+
+describe('listeners module', function () {
+  it('is accessible', function () {
+    function requireModule() {
+      require(MODULE_PATH);
+    }
+
+    expect(requireModule).not.to.throw;
+  });
+
+  it('exposes a function', function () {
+    var listeners = require(MODULE_PATH);
+
+    expect(listeners).to.be.a('function');
+  });
+}); // routes module
