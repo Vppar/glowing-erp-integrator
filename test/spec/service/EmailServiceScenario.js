cd06e893ef33bb2d0e366576fd647dc916954e2e
Rafael Oliveira Andrade <rafael.andrade@tuntscorp.com>
2014-06-27 11:34:42 -0300

    EmailService.sendApprovedPayment

--- test/spec/service/EmailServiceScenario.js
+++ test/spec/service/EmailServiceScenario.js
@@ -3,12 +3,13 @@ var assert = require("assert");
 
 describe('Service: EmailServiceScenario', function() {
 
-	it('should send email', function(done) {
+	it('should send billet email', function(done) {
 		var billetRequestSnapshot = {
 			"billetLink": "vpink.vc",
 			"consultant":{
 				"name": "Test VPink",
-				"email": "jarvis@tuntscorp.com"
+				//"email": "jarvis@tuntscorp.com"
+				"email": "rafael.andrade@tuntscorp.com"
 			}
 		};
 		EmailService.sendBilletToCustomer(billetRequestSnapshot, function(error, sucess) {
@@ -18,11 +19,35 @@ describe('Service: EmailServiceScenario', function() {
 		});
 	});
 
-	it('should not send email', function(done) {
+	it('should not send billet email', function(done) {
 		var billetRequestSnapshot = {
 			"consultant":{}
 		};
 		EmailService.sendBilletToCustomer(billetRequestSnapshot, function(error, sucess) {
+			done();
+			assert.equal('Invalid param', error);
+		});
+	});
+
+	it('should send billet email', function(done) {
+		var billetRequestSnapshot = {
+			"consultant":{
+				"name": "Test VPink",
+				"email": "rafael.andrade@tuntscorp.com"
+			}
+		};
+		EmailService.sendApprovedPayment(billetRequestSnapshot, function(error, sucess) {
+			done();
+			assert.equal(1, sucess.length);
+			assert.equal('sent', sucess[0].status);
+		});
+	});
+
+	it('should not send billet email', function(done) {
+		var billetRequestSnapshot = {
+			"consultant":{}
+		};
+		EmailService.sendApprovedPayment(billetRequestSnapshot, function(error, sucess) {
 			done();
 			assert.equal('Invalid param', error);
 		});
