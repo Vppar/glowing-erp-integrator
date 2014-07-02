6c30c0a594dabf109ba650dede7294be8cc0af61
Lucas Andrade <lucas.andrade999@gmail.com>
2014-07-02 11:40:13 -0300

    teste fluxo

--- test/bdini/simulate-subscription-consultant-request.js
+++ test/bdini/simulate-subscription-consultant-request.js
@@ -19,9 +19,7 @@ function add(subscriptionConsultantRequest) {
   });
 };
 
-
-
-var endereco = {};
+/*var endereco = {};
 endereco.tipo = 'RUA';
 endereco.logradouro = '15 de Novembro';
 endereco.numero = '1020';
@@ -43,19 +41,26 @@ consultant.emails = ['terceiro.teste@gmail.com'];
 consultant.rg = '42.943.412-1';
 consultant.dataNascimento = '12-08-1970';
 consultant.enderecos = [endereco];
-consultant.telefones = [telefone];
+consultant.telefones = [telefone];*/
 
+var address = {};
+address.street = 'rua 1';
+address.number = 555;
 
-/*consultant.address = address;
+var consultant = {};
+consultant.address = address;
 consultant.subscriptionExpirationDate = 1383066000000;
 consultant.uuid = '9999';
+consultant.name = 'Joao da Silva';
 consultant.cep = '12122999';
+consultant.cpf = '434.325.864-50';
+consultant.email = 'teste@teste.com';
 consultant.cityOrigin = 'Sao Paulo';
 consultant.countryOrigin = 'Brasil';
 consultant.complement = 'Apto 333';
 consultant.phone = '11 3333 4444';
 consultant.cellphone = '11 98444 2322';
-consultant.emailPrimer = 'dsfsdf@ddd.com';*/
+consultant.emailPrimer = 'dsfsdf@ddd.com';
 
 var subscriptionConsultantRequest = {};
 subscriptionConsultantRequest.uuid = 1111;
