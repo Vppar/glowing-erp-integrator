ced7bed3cc4db50f63871990fc0a8d163486216c
Lucas Andrade <lucas.andrade999@gmail.com>
2014-07-08 15:15:06 -0300

    customer test

--- test/spec/service/CustomerService/CustomerServiceReadSpec.js
+++ test/spec/service/CustomerService/CustomerServiceReadSpec.js
@@ -1,3 +1,38 @@
-/**
- * 
- */
+var CustomerService = require('../../../../lib/services/CustomerService');
+var assert = require('assert');
+
+describe('Service: CustomerService', function() {
+	
+	it('should get a customer by document', function(done) {
+		done();
+		
+		CustomerService.getByDocument('797.783.353-03', function(err, result){
+			assert.equal(err, null);
+			assert.notEqual(result, null);
+		});
+	});
+	
+	it('should get a result empty', function(done) {
+		done();
+		
+		CustomerService.getByDocument('797.783.353-12', function(err, result){
+			assert.equal(err, null);
+			assert.notEqual(result, null);
+			assert.equal(result, null);
+		});
+	});
+
+	it('should return error passing document invalid', function(done) {
+		done();
+		
+		CustomerService.getByDocument(null, function(err, result){
+			assert.notEqual(err, null);
+			assert.equal(result, null);
+		});
+		
+		CustomerService.getByDocument(10101010101010, function(err, result){
+			assert.notEqual(err, null);
+			assert.equal(result, null);
+		});
+	});
+});
--- test/spec/service/CustomerServiceTest.js
+++ test/spec/service/CustomerServiceTest.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var CustomerService = require('../../../../lib/services/CustomerService');
+var CustomerService = require('../../../lib/services/CustomerService');
 var expect = require('chai').expect;
 var should = require('chai').should();
 
@@ -11,36 +11,186 @@ describe('Service: CustomerServiceScenario', function() {
 	beforeEach(function() { 
 		customerValid = {
 			nome: 'Customer Name',
-			documento: '600.867.912-14',
-			emails: ['customername@teste.com']
-			enderecos: [{ logradouro: 'logradouro teste', numero: 45, bairro: 'bairro teste', cep: '12328460', codigoIBGECidade: 3549904}],
-			telefones: [{numero: '1239587489'}]
+			documento: '255.595.874-60',
+			enderecos: [{ tipo: 'AVENIDA', logradouro: 'logradouro teste', numero: '45', bairro: 'bairro teste', cep: '12328460', codigoIBGECidade: '3549904'}]
 		}	
 	});
 	
-	it('Should create a new customer', function(done){
-		done();
-	});
-	
 	//TODO: mock
-	it('Should get a customer by document', function(done){
-		CustomerService.getByDocument(001, '12.216.878/0009-03', function(err, result){
+	xit('Should get a customer by document', function(done){
+		CustomerService.getByDocument(1, '12.216.878/0009-03', function(err, result){
 			should.not.exist(err);
   			should.exist(result);
-  			result.should.be.an('object');
+  			result.should.be.an('string');
+  			
+  			var obj = JSON.parse(result);
+  			obj[0].should.be.an('object');
   			done();
-		})
-	});
+		});
+	})
 
 	//TODO: mock
-	it('Should get a empty object by document wrong', function(done){
-		CustomerService.getByDocument(001, '12.216.878/0009-03', function(err, result){
+	xit('Should get a empty object when document not exists', function(done){
+		CustomerService.getByDocument(1, '123', function(err, result){
 			should.not.exist(err);
   			should.exist(result);
-  			result.should.be.an('object');
+  			
+  			var obj = JSON.parse(result);
+  			expect(obj).to.be.empty;
   			done();
+		});
+	})
+	
+	//TODO: mock
+	xit('Should get error message callback when document is empty or not type of string', function(done){
+		CustomerService.getByDocument(1, null, function(err, result){
+			should.exist(err);
+			expect(result).to.be.undefined;
+  			err.should.be.an('string');
+		});
+		
+		CustomerService.getByDocument(1, 10, function(err, result){
+			should.exist(err);
+			expect(result).to.be.undefined;
+  			err.should.be.an('string');
+		});
+		
+		CustomerService.getByDocument(1, 10.2, function(err, result){
+			should.exist(err);
+			expect(result).to.be.undefined;
+  			err.should.be.an('string');
+		});
+		
+		CustomerService.getByDocument(1, true, function(err, result){
+			should.exist(err);
+  			should.not.exist(result);
+  			expect(result).to.be.undefined;
+  			err.should.be.an('string');
+		});
+		done();
+	})
+	
+	//TODO: mock
+	xit('Should create a new customer', function(done){
+		CustomerService.create(1, customerValid, function(err, result){
+			expect(err).to.be.null;
+			expect(result).not.to.be.undefined;
+			result.should.be.an('string');
+			
+			done();
+		})		
+	})
+	
+	xit('Should get error message callback when customer is empty', function(done){
+		CustomerService.create(1, null, function(err, result){
+			expect(err).not.to.be.null;
+			expect(result).to.be.undefined;
+			expect(err).to.be.equal('[CustomerService.create][Subscription: 1][Error: NewCustomer parameter is empty]');
+			done();
 		})
-	});
+	})
 	
+	xit('Should get error message callback when customer doesnt have name, document or address', function(done){
+		customerValid.nome = null;
+		
+		CustomerService.create(1, customerValid, function(err, result){
+			expect(err).not.to.be.null;
+			expect(result).to.be.undefined;
+			expect(err).to.be.equal('[CustomerService.create][Subscription: 1][Error: Check the following attributes: nome, documento and enderecos]');
+		})
+		
+		customerValid.nome = 'Customer Name';
+		customerValid.documento = null;
+		
+		CustomerService.create(1, customerValid, function(err, result){
+			expect(err).not.to.be.null;
+			expect(result).to.be.undefined;
+			expect(err).to.be.equal('[CustomerService.create][Subscription: 1][Error: Check the following attributes: nome, documento and enderecos]');
+		})
+		
+		customerValid.nome = 'Customer Name';
+		customerValid.documento = '255.595.874-60';
+		customerValid.enderecos = null;
+		
+		CustomerService.create(1, customerValid, function(err, result){
+			expect(err).not.to.be.null;
+			expect(result).to.be.undefined;
+			expect(err).to.be.equal('[CustomerService.create][Subscription: 1][Error: Check the following attributes: nome, documento and enderecos]');
+		})
+		
+		done();
+	})
 	
+	xit('Should get error message callback in mergeCustomer method when consultant is empty', function(done) {
+		var subscription = {
+			uuid: 1,
+			snapshot: { consultant: null }
+		};
+		
+		CustomerService.mergeCustomer(subscription, function(err, result) {
+			expect(err).not.to.be.null;
+			expect(result).to.be.undefined;
+			expect(err).to.be.equal('[CustomerService.mergeCustomer][Subscription: 1][Error: Customer parameter is invalid]');
+		})
+		done();
+	})
+	
+	//TODO: mock
+	it('Should create a new customer in mergeCustomer method when it doesnt exists in vpsa', function(done) {
+		
+		setTimeout(done, 2000);
+				
+		var subscription = {
+			uuid: 1,
+			snapshot: { 
+				consultant: {
+					name: 'unit test',
+					cpf: '332.328.226-49',
+					emailPrimer: 'unitteste@test.com',
+					email: 'unitteste2@test.com',
+					cep: '12328460',
+					phone: '1239548772',
+					cellphone: '1239640123',
+					address: {
+						street: 'avenida dom pedro'
+					}
+			}}
+		};
+		
+		CustomerService.mergeCustomer(subscription, function(err, result) {
+			
+			console.log(err);
+			console.log(result);
+			
+			/*expect(err).to.be.null;
+			expect(result).not.to.be.null;
+			result.should.be.an('string');	*/
+		
+			;
+		}).done();
+	})
+	
+	//TODO: mock
+	xit('Should update the customer in mergeCustomer method when it exists in vpsa', function(done) {
+		var obj = {};
+		
+		CustomerService.getByDocument(1, '12.216.878/0009-03', function(err, result) {
+			
+				obj = JSON.parse(result);
+				
+				
+				
+				
+				
+				
+				done();
+
+		});
+		
+	})
 });
+
+
+
+
+
